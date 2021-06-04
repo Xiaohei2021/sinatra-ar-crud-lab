901e98e4a04be146630912bb43480493c75d5ad3
Leon <hualiang.tian@gmail.com>
2021-06-03 22:37:19 -0400

    commit tree 7430987b0d39cb6c33f8c69f9388c7f61cf6f835

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,57 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  get '/' do
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+  #This is like the home page of a website
+  get '/articles' do
+    @articles = Article.all
+    # binding.pry
+    erb :index
   end
+#--------------------------------------------------------------------------------------------------
+  #This is will prompt/collect info from the developer for a new path. ***AKA new
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+#--------------------------------------------------------------------------------------------------
+  #This builds the new path. ***AKA create
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+     # user_input = params
+    # @article = Article.create(user_input)
+    # binding.pry
+    redirect to "/articles/#{ @article.id }"
+  end
+#--------------------------------------------------------------------------------------------------
+  #This shows the new path. ***AKA show
+  get '/articles/:id' do
+    # id = params[:id]
+    @article = Article.find(params[:id])
+    erb :show
+  end
+#--------------------------------------------------------------------------------------------------
+  #This allows user to edit the already built path. ***AKA edit
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+#--------------------------------------------------------------------------------------------------
+#This shows the newly updated path ***AKA update
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+
+  end
+#--------------------------------------------------------------------------------------------------
+#This allows path to be deleted *** AKA delete
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect to "/articles"
+  end
+
+
 end
+ 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,23 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    # @@all = []
+    # attr_accessor :title, :content
+
+    # def initialize(title, content)
+    #     @title = title
+    #     @content = content
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+
+    # def self.clear
+    #     @@all.clear
+    # end
+
+
 end
